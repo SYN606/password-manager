8c6f11d5c32ed48155d61f17c410f837e3f06c15
SYN <postbox.501@yandex.com>
2023-04-15 21:58:02 +0530

    created UI and added show and hide functions

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ import eel
 
 eel.init("web")
 
-@eel.expose
+@eel.expose # decorater
 def get_data():
     print("Function is running succesfully")
-    return "Got data from python"
+    return "Hello world"
+
+@eel.expose
+def show_passwords():
+    print("Show passwords is running successfully")
+    return "This is a Password"
 
 eel.start(  
             "index.html", 
--- web/script.js
+++ web/script.js
@@ -1,12 +1,43 @@
 console.log("Hello world")
+const genPassBtn = document.getElementById("gen-pass");
+const newPass = document.querySelector(".generate_new_password");
 
+genPassBtn.addEventListener("click", e => {
+    if (newPass.hidden) {
+        newPass.hidden = false;
+    }
+    else {
+        newPass.hidden = true;
+    }
+});
+const customPassBtn = document.getElementById("custom-password");
+const customPass = document.querySelector(".custom-password");
 
-document.getElementById('home').addEventListener('click', () => {
-    getDataFromPython();
-    // document.getElementById('output').innerText = "password manager"
+customPassBtn.addEventListener("click", e => {
+    if (customPass.hidden) {
+        customPass.hidden = false;
+    }
+    else {
+        customPass.hidden = true;
+    }
+});
+const showPassBtn = document.getElementById("show_pass")
+const showPass = document.querySelector(".view_all_passwords");
+
+showPassBtn.addEventListener("click", e => {
+    if (showPass.hidden) {
+        showPass.hidden = false;
+    }
+    else {
+        showPass.hidden = true;
+    }
 });
 
+const copyEmail = document.querySelectorAll(".copy_email");
+const copyPassword = document.querySelectorAll(".copy_password");
 
-async function getDataFromPython() {
-    document.getElementById('output').innerText = await eel.get_data()();
-}
+copyEmail.forEach(item => {
+    item.addEventListener("click", e=>{
+        navigator.clipboard.writeText(item.innerText);
+    })
+});
